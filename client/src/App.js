6435cb6c9b60745d4eed6bc67b3888fd4c207287
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-04 11:38:30 -0500

    add some files and create homepage file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
-import NotFound from './pages/NotFound';
+import Home from './pages/Homepage';
+// import Matchup from './pages/Matchup';
+// import Vote from './pages/Vote';
+
 
 const client = new ApolloClient({
   uri: '/graphql',
@@ -15,20 +15,17 @@ function App() {
   return (
     <ApolloProvider client={client}>
       <Router>
-        <div className="flex-column justify-center align-center min-100-vh bg-primary">
+        <div className="container">
           <Switch>
             <Route exact path="/">
               <Home />
             </Route>
-            <Route exact path="/matchup">
+            {/* <Route exact path="/matchup">
               <Matchup />
             </Route>
             <Route exact path="/matchup/:id">
               <Vote />
-            </Route>
-            <Route>
-              <NotFound />
-            </Route>
+            </Route> */}
           </Switch>
         </div>
       </Router>
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Homepage = () => {
+  return (
+    <div className="homepage-wrapper">
+      <div className="homepage-heading">
+        <h1>Welcome to Weekly Shedular</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Homepage;
