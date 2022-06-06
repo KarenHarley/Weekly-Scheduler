d0f9f6c561f078c8ddac733130f34db42a707311
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-06 11:20:31 -0500

    add task typeDef query

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,27 +1,21 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Tech {
+  type Task { 
     _id: ID!
     name: String!
+    notes: String
+    startingTime!
+    endingTime!
   }
 
-  type Matchup {
-    _id: ID!
-    tech1: String!
-    tech2: String!
-    tech1_votes: Int
-    tech2_votes: Int
-  }
 
   type Query {
-    tech: [Tech]
-    matchups(_id: String): [Matchup]
+    tasks: [Task]
   }
 
   type Mutation {
-    createMatchup(tech1: String!, tech2: String!): Matchup
-    createVote(_id: String!, techNum: Int!): Matchup
+
   }
 `;
 
