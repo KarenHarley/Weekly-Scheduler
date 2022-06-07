260fb05ea32e21cc257fac3794752c09c17c6e57
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-07 13:28:39 -0500

    pass in data as props and display it

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
 
-const Task = () => {
+const Task = ({ data }) => {
+  console.log(data);
   return (
     <div className="task-wrapper">
       <div className="task-heading">
         <p>This is a single task</p>
+        <p>Name: {data.name}</p>
+        <p>Starting Time: {data.startingTime}</p>
+        <p>Ends: {data.endingTime}</p>
+        <p>User: {data.user.username}</p>
       </div>
     </div>
   );
--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
-import { useQuery } from '@apollo/client';
-import { QUERY_TASKS } from '../utils/queries';
+import { useQuery } from "@apollo/client";
+import { QUERY_TASKS } from "../utils/queries";
 
 import Task from "../components/Task";
 const Tasks = () => {
+  let id = "629e57ed0abac12714b8d215";
 
-    let id = "629e57ed0abac12714b8d215";
+  const { loading, data } = useQuery(QUERY_TASKS, {
+    variables: {
+      userId: id,
+    },
+  });
 
-    const { loading, data } = useQuery(QUERY_TASKS, {
-        variables: {
-            userId: id,
-          },
-      });
-      console.log(data)
+  console.log(data);
+
+  const tasks = data?.tasks || [];
+
   return (
     <div className="tasks-wrapper">
       <div className="task-heading">
         <h1>Welcome to the Tasks Page</h1>
-        <Task />
+        {loading ? (
+          <div>Loading...</div>
+        ) : (
+          <div>
+            {tasks.map((task) => {
+              return <Task data={task} />;
+            })}
+          </div>
+        )}
       </div>
     </div>
   );
