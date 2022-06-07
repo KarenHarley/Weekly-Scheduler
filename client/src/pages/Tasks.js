a98b374703935f7ac320e51f6b5a215f8e90cd1c
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-07 12:59:25 -0500

    use query to get all tasks

--- client/src/pages/Tasks.js
+++ client/src/pages/Tasks.js
@@ -1,6 +1,18 @@
 import { Link } from "react-router-dom";
+import { useQuery } from '@apollo/client';
+import { QUERY_TASKS } from '../utils/queries';
+
 import Task from "../components/Task";
 const Tasks = () => {
+
+    let id = "629e57ed0abac12714b8d215";
+
+    const { loading, data } = useQuery(QUERY_TASKS, {
+        variables: {
+            userId: id,
+          },
+      });
+      console.log(data)
   return (
     <div className="tasks-wrapper">
       <div className="task-heading">
