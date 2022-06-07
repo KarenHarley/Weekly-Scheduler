4ca291487b70bd93ddaf0ca78cbb6e207c821d08
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-06 19:20:25 -0500

    add mutations to file for creating and updating tasks

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,2 +1,53 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
+export const updateTask = qql`
+mutation updateTask(
+    $name: String!
+     $notes: String
+     $startingTime: String!
+     $endingTime: String!
+    $_id: String!
+ ) {
+   updateTask(
+    name: $name
+     notes: $notes
+     startingTime: $startingTime
+     endingTime: $endingTime
+     _id: $_id
+   ) {
+     name,
+     notes,
+     startingTime,
+     endingTime,
+     user {
+           _id
+     }
+   }
+ }
+ `;
+
+export const createTask = qql`
+ mutation addTask(
+    $name: String!
+     $notes: String
+     $startingTime: String!
+     $endingTime: String!
+    $user: ID!
+ ) {
+   addTask(
+    name: $name
+     notes: $notes
+     startingTime: $startingTime
+     endingTime: $endingTime
+     user: $user
+   ) {
+     name,
+     notes,
+     startingTime,
+     endingTime,
+     user {
+           _id
+     }
+   }
+ }
+ `;
