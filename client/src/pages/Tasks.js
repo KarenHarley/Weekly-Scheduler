a7b5a224624db18e75561425749f44e8f0c88c6d
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-07 12:44:24 -0500

    add basic setup for tasks page

---
+++ client/src/pages/Tasks.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const Tasks = () => {
+  return (
+    <div className="tasks-wrapper">
+      <div className="task-heading">
+        <h1>Welcome to the Tasks Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
