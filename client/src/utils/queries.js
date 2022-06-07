70e016348479450a702eeb657dda89586ebddda5
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-07 12:53:49 -0500

    add query tasks in queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,2 +1,16 @@
 import { gql } from '@apollo/client';
 
+export const QUERY_TASKS = gql`
+
+query tasks($userId: ID!) {
+    tasks(userId: $userId ) {
+     name,
+     notes,
+    startingTime,
+    endingTime,
+    user{
+      username
+    }
+    }
+  }
+`;
