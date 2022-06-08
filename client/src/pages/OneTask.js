6cd34e3cdd6b05d9f57c86b28ee5ba8198d489c3
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-08 17:05:53 -0500

    add one task page

---
+++ client/src/pages/OneTask.js
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+const OneTask = () => {
+  return (
+    <div className="task-wrapper">
+      <div className="task-heading">
+        <h1>One task</h1>
+      </div>
+    </div>
+  );
+};
+
+export default OneTask;
