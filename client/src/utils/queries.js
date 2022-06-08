db51a3eef81b783826ca5590655d6d11def62757
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-08 16:43:35 -0500

    get id from query

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,16 +1,16 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const QUERY_TASKS = gql`
-
-query tasks($userId: ID!) {
-    tasks(userId: $userId ) {
-     name,
-     notes,
-    startingTime,
-    endingTime,
-    user{
-      username
-    }
+  query tasks($userId: ID!) {
+    tasks(userId: $userId) {
+      _id
+      name
+      notes
+      startingTime
+      endingTime
+      user {
+        username
+      }
     }
   }
 `;
