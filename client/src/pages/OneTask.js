fe81302dc221255441cb10c5bd2fa27f008e0c73
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-08 17:20:14 -0500

    get id from url

--- client/src/pages/OneTask.js
+++ client/src/pages/OneTask.js
@@ -1,10 +1,22 @@
 import { Link } from "react-router-dom";
-
+import { useQuery } from "@apollo/client";
+import { QUERY_TASKS } from "../utils/queries";
+import { useParams } from "react-router-dom";
 const OneTask = () => {
+  const params = useParams();
+  console.log(params.id);
+  
+  const { loading, data } = useQuery(QUERY_TASKS, {
+    variables: {
+      userId: id,
+    },
+  });
+  console.log(data);
   return (
     <div className="task-wrapper">
       <div className="task-heading">
         <h1>One task</h1>
+        {loading ? <div>Loading...</div> : <div></div>}
       </div>
     </div>
   );
