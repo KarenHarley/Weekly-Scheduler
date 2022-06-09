deceffac6c2525414bce7a83e08b7f215bf7f48e
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-08 20:34:25 -0500

    add a time array and create option for select

--- client/src/pages/EditTask.js
+++ client/src/pages/EditTask.js
@@ -14,7 +14,40 @@ const EditTask = () => {
 
   const [taskName, setTaskName] = useState(data.task.name);
   const [taskNotes, setTaskNotes] = useState(data.task.notes);
+  const [taskStart, setTaskStart] = useState(data.task.startingTime);
   console.log(data);
+
+  const times = [
+    "1:00 am",
+    "2:00 am",
+    "3:00 am",
+    "4:00 am",
+    "5:00 am",
+    "6:00 am",
+    "7:00 am",
+    "8:00 am",
+    "9:00 am",
+    "10:00 am",
+    "11:00 am",
+    "12:00 am",
+    "1:00 pm",
+    "2:00 pm",
+    "3:00 pm",
+    "4:00 pm",
+    "5:00 pm",
+    "6:00 pm",
+    "7:00 pm",
+    "8:00 pm",
+    "9:00 pm",
+    "10:00 pm",
+    "11:00 pm",
+    "12:00 pm",
+  ];
+
+  const startTimeChange = (e) => {
+    console.log(e.target.value);
+    setTaskStart(e.target.value);
+  };
   return (
     <div className="edit-task-wrapper">
       <div className="edit-task-heading">
@@ -31,6 +64,27 @@ const EditTask = () => {
             <Link to={`/task/${data.task._id}`}>Back to Task</Link>
 
             <form>
+              <label>
+                Select Starting Time
+                <select value={taskStart} onChange={startTimeChange}>
+                  {times.map((time) => {
+                    //set value to 1 in "1:00 am"
+                    let optionValue = time.split(":")[0];
+                    let optionKey = time.split(":")[0];
+                    //if the string has "pm" set value to 13 in  "1 pm"
+                    if (time.split(" ")[1] === "pm") {
+                      optionValue = Number(time.split(":")[0]) + 12;
+                      optionKey = Number(time.split(":")[0]) + 12;
+                    }
+
+                    return (
+                      <option value={optionValue} key={optionKey}>
+                        {time}
+                      </option>
+                    );
+                  })}
+                </select>
+              </label>
               <label>
                 Name:
                 <input type="text" name="name" value={taskName} />
