7a929a4650a2656fa69aaaf100fce09222c5b0bc
KarenHarley <karen.pretzel.365@gmail.com>
2022-06-11 10:20:54 -0500

    add :30 option and add a value of .5

--- client/src/pages/EditTask.js
+++ client/src/pages/EditTask.js
@@ -4,7 +4,6 @@ import { QUERY_TASK } from "../utils/queries";
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 const EditTask = () => {
-  
   const params = useParams();
 
   const { loading, data } = useQuery(QUERY_TASK, {
@@ -20,28 +19,51 @@ const EditTask = () => {
 
   const times = [
     "1:00 am",
+    "1:30 am",
     "2:00 am",
+    "2:30 am",
     "3:00 am",
+    "3:30 am",
     "4:00 am",
+    "4:30 am",
     "5:00 am",
+    "5:30 am",
     "6:00 am",
+    "6:30 am",
     "7:00 am",
+    "7:30 am",
     "8:00 am",
+    "8:30 am",
     "9:00 am",
+    "9:30 am",
     "10:00 am",
+    "10:30 am",
     "11:00 am",
+    "11:30 am",
     "12:00 am",
+    "12:30 am",
     "1:00 pm",
+    "1:30 pm",
     "2:00 pm",
+    "2:30 pm",
     "3:00 pm",
+    "3:30 pm",
     "4:00 pm",
+    "4:30 pm",
     "5:00 pm",
+    "5:30 pm",
     "6:00 pm",
+    "6:30 pm",
     "7:00 pm",
+    "7:30 pm",
     "8:00 pm",
+    "8:30 pm",
     "9:00 pm",
+    "9:30 pm",
     "10:00 pm",
+    "10:30 pm",
     "11:00 pm",
+    "11:30 pm",
     "12:00 pm",
   ];
 
@@ -72,6 +94,17 @@ const EditTask = () => {
       optionKey = Number(time.split(":")[0]) + 12;
     }
 
+    if (
+      time.split(" ")[0].split(":")[1] === "30" &&
+      time.split(" ")[1] === "am"
+    ) {
+      optionValue = Number(time.split(":")[0]) + 0.5;
+    } else if (
+      time.split(" ")[0].split(":")[1] === "30" &&
+      time.split(" ")[1] === "pm"
+    ) {
+      optionValue = Number(time.split(":")[0]) + 12.5;
+    }
     return (
       <option value={optionValue} key={optionKey}>
         {time}
