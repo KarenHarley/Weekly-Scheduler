77612e42ec7ac8c443a697e7e2cbe39c97f9bbbf
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-03 13:27:32 -0500

    create createTask basic structure

---
+++ client/src/pages/CreateTask.js
@@ -0,0 +1,17 @@
+
+import { useQuery } from "@apollo/client";
+//import { QUERY_TASKS } from "../utils/queries";
+import {Link } from "react-router-dom";
+//import Task from "../components/Task";
+
+const Tasks = () => {
+  return (
+    <div className="create-task-wrapper">
+      <div className="create-task-heading">
+        <h1>Welcome to the Create Tasks Page</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Tasks;
