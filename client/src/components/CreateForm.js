45d7f62f668229379087ba2a67ae2244d192d145
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-03 13:31:52 -0500

    add basic structure for form

---
+++ client/src/components/CreateForm.js
@@ -0,0 +1,12 @@
+
+const CreateForm = () => {
+  return (
+    <div className="create-form-wrapper">
+      <div className="create-form-heading">
+        <h1>Create Form</h1>
+      </div>
+    </div>
+  );
+};
+
+export default CreateForm;
