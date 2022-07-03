3b3ac794709dae6d706a352b7ff2af6852638ee4
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-03 12:28:30 -0500

    change login JSX

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,41 +3,41 @@ import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../../../utils/mutations";
 
 const Login = (props) => {
-    const classes = useStyles();
-    const [formState, setFormState] = useState({ email: "", password: "" });
-    const [login, { error, data }] = useMutation(LOGIN_USER);
-  
-    const handleChange = (e) => {
-      const { name, value } = e.target;
-  
-      setFormState({
-        ...formState,
-        [name]: value,
+  const classes = useStyles();
+  const [formState, setFormState] = useState({ email: "", password: "" });
+  const [login, { error, data }] = useMutation(LOGIN_USER);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormState({
+      ...formState,
+      [name]: value,
+    });
+  };
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    console.log(formState);
+
+    try {
+      const { data } = await login({
+        variables: { ...formState },
       });
-    };
-    const handleFormSubmit  = async (e) => {
-      e.preventDefault();
-      console.log(formState);
-  
-      try {
-        const { data } = await login({
-          variables: { ...formState },
-        });
-  
-        Auth.login(data.login.token);
-        window.location.replace("/lista_contacto");
-      } catch (e) {
-        console.error(e);
-      }
-  
-      // clear form values
-      setFormState({
-        email: "",
-        password: "",
-      });
-    };
-    return (
-      <div class="login-form">
+
+      Auth.login(data.login.token);
+      window.location.replace("/lista_contacto");
+    } catch (e) {
+      console.error(e);
+    }
+
+    // clear form values
+    setFormState({
+      email: "",
+      password: "",
+    });
+  };
+  return (
+    <div class="user-form">
       <h1>Login</h1>
       <form>
         <input
@@ -56,10 +56,12 @@ const Login = (props) => {
           value={formState.password}
           required
         />
-        <input type="submit" />
-        <p>Don't have an account? <a href="/signUp">Sign up</a></p>
+        <input type="submit" onClick={handleFormSubmit} />
+        <p>
+          Don't have an account? <a href="/signup">Sign up</a>
+        </p>
       </form>
     </div>
-    );
-  };
-  export default Login;
+  );
+};
+export default Login;
