64a9abd58015b8e0cfd920b31f5044c714b8a739
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-04 15:47:27 -0500

    add context to out server to use the authMiddleware function

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
-const express = require('express');
-const path = require('path');
-const { ApolloServer } = require('apollo-server-express');
-const db = require('./config/connection');
-const { typeDefs, resolvers } = require('./schemas');
-
+const express = require("express");
+const path = require("path");
+const { ApolloServer } = require("apollo-server-express");
+const db = require("./config/connection");
+const { typeDefs, resolvers } = require("./schemas");
+// Import `authMiddleware()` function to be configured with the Apollo Server
+const { authMiddleware } = require("./utils/auth");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  // Add context to our server so data from the `authMiddleware()` function can pass data to our resolver functions
+  context: authMiddleware, //need to call it context...now we can access preces of info from authMiddleware in resolvers (see resolvers)
 });
 
 server.applyMiddleware({ app });
@@ -17,15 +20,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
-db.once('open', () => {
+db.once("open", () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
     // log where we can go to test our GQL API
