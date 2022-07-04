ab5ff03847ce574ca2c08679a6add1f9aaa0b0b2
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-04 16:42:37 -0500

    create basic form

--- client/src/pages/CreateTask.js
+++ client/src/pages/CreateTask.js
@@ -16,6 +16,49 @@ const CreateTask = () => {
       <div className="create-task-heading">
         <h1>Welcome to the Create Tasks Page</h1>
       </div>
+
+      <form>
+        <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          id="task-name"
+          value={formState.name}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="notes"
+          placeholder="Notes"
+          id="task-notes"
+          value={formState.notes}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="startingTime"
+          placeholder="Start"
+          id="task-startingTime"
+          value={formState.startingTime}
+          onChange={handleChange}
+          required
+        />
+        <input
+          type="text"
+          name="endingTime"
+          placeholder="End"
+          id="task-endingTime"
+          value={formState.endingTime}
+          onChange={handleChange}
+          required
+        />
+        <input type="submit" onClick={handleFormSubmit} />
+        <p>
+          Don't have an account? <a href="/signup">Sign up</a>
+        </p>
+      </form>
     </div>
   );
 };
