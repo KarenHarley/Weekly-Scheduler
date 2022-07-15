d3727f17d02704ad834ae94e966cf9c96fb4714a
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-15 16:47:37 -0500

    add img to homepage and size it

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -2,11 +2,11 @@ import { Link } from "react-router-dom";
 import Auth from "../utils/auth";
 
 const Homepage = () => {
-
   return (
     <div className="homepage-wrapper">
       <div className="homepage-heading">
         <h1>Welcome to Weekly Shedular</h1>
+        <img src="./Pics/banner.jpg"></img>
       </div>
     </div>
   );
