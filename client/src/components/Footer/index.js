0dbb3619bc937ef1e4de31ede74c21ecbe062e0f
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-15 21:50:56 -0500

    create footer basic structure

---
+++ client/src/components/Footer/index.js
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Footer = ({ data }) => {
+  console.log(data);
+  return (
+    <div className="footer-wrapper">
+      This is the footer
+    </div>
+  );
+};
+
+export default Footer;
