9b93b134cd2ab36a770d627512e81d00c9661cf8
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-18 16:55:26 -0500

    add strategy text

--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -29,15 +29,24 @@ const Homepage = () => {
         <h4>Schedule</h4>
         <p>
           {" "}
-          Scheduling which is an aspect of organization takes things up a notch. It helps you get clear on what your purpose and objectives are. Rather than just spinning wheels, having a good schedule ensures that all our goals are identified and will be tackled. Scheduling also fights against for crastination by having a specific time frame to complete a task. 
-         {" "}
+          Scheduling which is an aspect of organization takes things up a notch.
+          It helps you get clear on what your purpose and objectives are. Rather
+          than just spinning wheels, having a good schedule ensures that all our
+          goals are identified and will be tackled. Scheduling also fights
+          against for crastination by having a specific time frame to complete a
+          task.{" "}
         </p>
       </div>
       <div className="plan">
         <h4>Strategize</h4>
         <p>
           {" "}
-          {" "}
+          Throughout history, strategy has been a key element in the success of
+          major battles. Today, a strategic mindset can help us to plan in a way
+          in which we can foresee the future and prepare accordingly with sense
+          of direction. Nevertheless, it’s important to remember that any good
+          strategy needs to be put on a roadmap. This is exactly how strategy is
+          linked to scheduling and organization.{" "}
         </p>
       </div>
     </div>
