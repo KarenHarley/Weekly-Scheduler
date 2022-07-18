621a186f0ffee43f290c40d3df38498ae8da7f5c
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-18 17:26:57 -0500

    fix height issue of login-please

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,8 +1,30 @@
 import { Link, useLocation } from "react-router-dom";
-
+import { useEffect } from "react";
 const Footer = ({ data }) => {
   console.log(data);
   const location = useLocation();
+
+  useEffect(() => {
+    // If navbar is not used to change page (like if a link is used)
+    //state will still be updated
+    switch (location.pathname) {
+      case "/tasks":
+        console.log(location);
+        return;
+      case "/login":
+        console.log(location);
+        return;
+      case "/signup":
+        console.log(location);
+        return;
+      case "/":
+        console.log(location);
+        return;
+      case "/account":
+        console.log(location);
+        return;
+    }
+  }, [location]);
   return (
     <div className="footer-wrapper">
       <ul>
