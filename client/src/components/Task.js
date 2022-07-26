d277736b2283399ddcbf35c8859885333edf7867
KarenHarley <karen.pretzel.365@gmail.com>
2022-07-26 16:42:10 -0500

    add checkbox for task

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -1,14 +1,22 @@
 import { Link } from "react-router-dom";
 import { formatTime } from "../utils/utils";
+import { useState, useEffect } from "react";
 const Task = ({ data }) => {
   console.log(data);
+  const [checked, setChecked] = useState(false);
+
+  const handleChange = () => {
+    setChecked(!checked);
+  };
   return (
     <div
       className={
         data.quadrant ? `${data.quadrant} task-wrapper` : "task-wrapper"
       }
     >
+      <input type="checkbox" checked={checked} onChange={handleChange} />
       <p>
+        {data.completed ? <span>{data.quadrant}</span> : <></>}
         {data.quadrant ? <span>{data.quadrant}</span> : <></>}
         <span>
           {formatTime(data.startingTime)}-{formatTime(data.endingTime)}
