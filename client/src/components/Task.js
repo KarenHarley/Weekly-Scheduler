31b8013c33fb23eb6c43e7e37ce4ddead97935dd
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-08 20:27:51 -0500

    clean code in task.js

--- client/src/components/Task.js
+++ client/src/components/Task.js
@@ -4,34 +4,24 @@ import { useMutation } from "@apollo/client";
 import { useState, useEffect } from "react";
 import { CHANGE_COMPLETED } from "../utils/mutations";
 const Task = ({ data }) => {
-  console.log(data);
   const [changeCompleted, { error, changeCompletedData }] =
     useMutation(CHANGE_COMPLETED);
 
   const ChangeCompleted = async (taskId, checked) => {
     try {
-      console.log(checked);
       const { updateCompleted } = await changeCompleted({
         variables: { completed: checked, _id: taskId },
       });
-      console.log(updateCompleted);
-      console.log(checked);
       return checked;
     } catch (e) {
       console.error(e);
     }
   };
   const handleChange = async (e) => {
-    console.log("checked", e.target.checked);
     const change = await ChangeCompleted(e.target.id, e.target.checked);
     e.target.checked = change;
   };
 
-  // useEffect(() => {
-  //   if (data) {
-  //     setChecked(data.completed);
-  //   }
-  // }, [data]);
   return (
     <div
       className={
@@ -48,7 +38,6 @@ const Task = ({ data }) => {
             onChange={handleChange}
           />
         </span>
-        {/* {data.completed ? <span>{data.quadrant}</span> : <></>} */}
         {data.quadrant ? <span>{data.quadrant}</span> : <span>Task</span>}
         <span>
           {formatTime(data.startingTime)}-{formatTime(data.endingTime)}
