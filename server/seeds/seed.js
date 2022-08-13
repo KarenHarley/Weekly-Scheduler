09371590f98f090f051fe1e69c943bed721000c5
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-12 20:16:09 -0500

    add step to seed.js

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,11 +1,12 @@
 const db = require("../config/connection");
-const { User,Task } = require("../models");
+const { User,Task,Step } = require("../models");
 
 const userData = require("./userData.json");
 
 db.once("open", async () => {
   await User.deleteMany({});
   await Task.deleteMany({})
+  await Step.deleteMany({})
 
   const users = await User.insertMany(userData);
 
