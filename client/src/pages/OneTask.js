bb47b1a8226bbeae72b3f842a4e52491f7ef12dd
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-19 16:39:25 -0500

    inport task card component in onetask file

--- client/src/pages/OneTask.js
+++ client/src/pages/OneTask.js
@@ -5,7 +5,7 @@ import { useParams } from "react-router-dom";
 import { formatTime } from "../utils/utils";
 import CreateForm from "../components/CreateForm";
 import Auth from "../utils/auth";
-import Task from "../components/Task";
+import TaskCard from "../components/TaskCard";
 
 const OneTask = () => {
   const params = useParams();
@@ -33,52 +33,7 @@ const OneTask = () => {
     <div className="one-task-wrapper">
       {" "}
       {Auth.loggedIn() ? (
-        <div className="steps-and-task-container">
-          {loading ? (
-            <div>Loading...</div>
-          ) : (
-            <>
-              {data.task.steps != 0 ? (
-                <div className="create-step-form">
-                  <CreateForm />
-                </div>
-              ) : (
-                <></>
-              )}
-              <div className="one-task">
-                <p>
-                  {data.task.quadrant ? (
-                    <span>{data.task.quadrant}</span>
-                  ) : (
-                    <></>
-                  )}
-                  {formatTime(data.task.startingTime)}-
-                  {formatTime(data.task.endingTime)}
-                </p>
-                <p>Name: {data.task.name}</p>
-                <div className="notes-div">
-                  <h4>Notes:</h4>
-                  <p>{data.task.notes}</p>
-                </div>
-                <Link to={`/task/edit/${data.task._id}`}>Edit</Link>
-              </div>
-              {data.task.steps != 0 ? (
-                <div className="step-container">
-                  <h4>Steps or Smaller tasks to Accomplish this larger task</h4>
-                  {data.task.steps.map((loopingStep, i) => {
-                    return (
-                      // <li type="li" id={loopingStep.id} key={loopingStep.id}>
-                      <Task data={loopingStep} key={i} />
-                      // </li>
-                    );
-                  })}
-                </div>
-              ) : (
-                <></>
-              )}
-            </>
-          )}
-        </div>
+        <TaskCard />
       ) : (
         <p className="login-please">
           You need to be logged in to see this Task. Please{" "}
