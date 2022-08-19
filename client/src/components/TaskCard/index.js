7f0f71b7c2f9e141333de960be30249463209e06
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-19 16:41:06 -0500

    create basic structure of taskCard

---
+++ client/src/components/TaskCard/index.js
@@ -0,0 +1,15 @@
+import { Link, useLocation } from "react-router-dom";
+import { formatTime } from "../../utils/utils";
+import { useMutation } from "@apollo/client";
+import { useState, useEffect } from "react";
+
+const TaskCard = ({ data }) => {
+
+  return (
+    <div>
+    
+    </div>
+  );
+};
+
+export default TaskCard;
