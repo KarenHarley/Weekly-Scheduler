3baad50e83cbf6846961db624d61543895814fd5
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-23 11:32:03 -0500

    add icon to jsx

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -4,6 +4,7 @@ import { useState, useEffect } from "react";
 const Navbar = () => {
   const location = useLocation();
   const [navLink, setNavLink] = useState("");
+  const [navbarOpen, setNavbarOpen] = useState(false);
   const logout = () => {
     setNavLink("");
     Auth.logout();
@@ -42,61 +43,67 @@ const Navbar = () => {
         <Link className="link" to="/">
           Weekly Shedular
         </Link>
-
-        <ul className="navbar-ul" onClick={selectPage}>
-          <li className={navLink === "See All Tasks" ? "selected" : "nav-link"}>
-            <Link
-              id="See All Tasks"
-              to="/tasks"
-              style={{ textDecoration: "none" }}
-              className="link"
+        <div className="link-menu">
+          <div className="hamburger-icon-div">
+            <i class="bx bx-menu"></i>
+          </div>
+          <ul className="navbar-ul" onClick={selectPage}>
+            <li
+              className={navLink === "See All Tasks" ? "selected" : "nav-link"}
             >
-              See All Tasks
-            </Link>
-          </li>
-          {Auth.loggedIn() ? (
-            <>
-              <li className={navLink === "Account" ? "selected" : "nav-link"}>
-                <Link
-                  id="Account"
-                  to="/account"
-                  style={{ textDecoration: "none" }}
-                  className="link"
-                >
-                  Account
-                </Link>
-              </li>
-              <li>
-                <button className="link" onClick={logout}>
-                  Logout
-                </button>
-              </li>
-            </>
-          ) : (
-            <>
-              <li className={navLink === "Login" ? "selected" : "nav-link"}>
-                <Link
-                  id="Login"
-                  to="/login"
-                  className="link"
-                  style={{ textDecoration: "none" }}
-                >
-                  Login
-                </Link>
-              </li>
-              <li className={navLink === "Signup" ? "selected" : "nav-link"}>
-                <Link
-                  id="Signup"
-                  to="/signup"
-                  className="link"
-                  style={{ textDecoration: "none" }}
-                >
-                  Sign up
-                </Link>
-              </li>
-            </>
-          )}
-        </ul>
+              <Link
+                id="See All Tasks"
+                to="/tasks"
+                style={{ textDecoration: "none" }}
+                className="link"
+              >
+                See All Tasks
+              </Link>
+            </li>
+            {Auth.loggedIn() ? (
+              <>
+                <li className={navLink === "Account" ? "selected" : "nav-link"}>
+                  <Link
+                    id="Account"
+                    to="/account"
+                    style={{ textDecoration: "none" }}
+                    className="link"
+                  >
+                    Account
+                  </Link>
+                </li>
+                <li>
+                  <button className="link" onClick={logout}>
+                    Logout
+                  </button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li className={navLink === "Login" ? "selected" : "nav-link"}>
+                  <Link
+                    id="Login"
+                    to="/login"
+                    className="link"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Login
+                  </Link>
+                </li>
+                <li className={navLink === "Signup" ? "selected" : "nav-link"}>
+                  <Link
+                    id="Signup"
+                    to="/signup"
+                    className="link"
+                    style={{ textDecoration: "none" }}
+                  >
+                    Sign up
+                  </Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </div>
   );
