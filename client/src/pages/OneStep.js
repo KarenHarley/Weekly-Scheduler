01cffd2f3033d50c722e5f3d9581208c7112d7a0
KarenHarley <karen.pretzel.365@gmail.com>
2022-08-26 22:02:43 -0500

    add a back link in one step

--- client/src/pages/OneStep.js
+++ client/src/pages/OneStep.js
@@ -49,6 +49,7 @@ const OneStep = () => {
                   <p>{data.step.notes}</p>
                 </div>
                 <Link to={`/step/edit/${data.step._id}`}>Edit</Link>
+                <Link to={`/task/${data.step._id}`}>Back</Link>
               </div>
             </>
           )}
