df39c0c3a37ba8bdec15f198cfc29bb2cf7d9e40
KarenHarley <karen.pretzel.365@gmail.com>
2022-09-23 20:58:26 -0500

    fix typo in database name

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weekly-shedular', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/weekly-schedular', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
